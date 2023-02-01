b972248f8a78dab2149546e531dd6b0870727fa1
abdalla mansour <abdallamansour.com>
2023-02-01 10:37:09 +0200

    upload files

---
+++ js/script.js
@@ -0,0 +1,48 @@
+gsap.from(".nav-items > *", 1.4, {
+    opacity: 0,
+    x: 40,
+    ease: "power3.inOut",
+    stagger: 0.2,
+});
+
+gsap.from(".header", 1.4, {
+    opacity: 0,
+    x: 80,
+    ease: "power3.inOut",
+    delay: 0.2,
+});
+
+gsap.from(".slider-copy > *", 1.4, {
+    opacity: 0,
+    x: 40,
+    ease: "power3.inOut",
+    stagger: 0.2,
+});
+
+gsap.from(".slider-hr", 1.4, {
+    x: "120%",
+    ease: "power3.inOut",
+    delay: 0.4,
+});
+
+gsap.from(".bottom-nav > .about > .country > *", 1.4, {
+    opacity: 0,
+    x: 40,
+    ease: "power3.inOut",
+    stagger: 0.2,
+});
+
+gsap.from(".bottom-nav > .about > .year > *", 1.4, {
+    opacity: 0,
+    x: 40,
+    ease: "power3.inOut",
+    delay: 0.4,
+    stagger: 0.2,
+});
+
+gsap.from(".box", 1.4, {
+    opacity: 0,
+    x: 40,
+    ease: "power3.inOut",
+    stagger: 0.2,
+});
